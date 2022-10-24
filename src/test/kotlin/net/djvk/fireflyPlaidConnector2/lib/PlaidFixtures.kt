a5ef6e10febd4563a852802663de72842a37b2ed
Dan van Kley <dvankley@gmail.com>
2022-10-23 20:00:43 -0400

    Missed a spot

--- src/test/kotlin/net/djvk/fireflyPlaidConnector2/lib/PlaidFixtures.kt
+++ src/test/kotlin/net/djvk/fireflyPlaidConnector2/lib/PlaidFixtures.kt
@@ -1,6 +1,7 @@
 package net.djvk.fireflyPlaidConnector2.lib
 
 import net.djvk.fireflyPlaidConnector2.api.plaid.models.*
+import net.djvk.fireflyPlaidConnector2.util.Utilities
 import java.time.LocalDate
 
 object PlaidFixtures {
@@ -73,6 +74,78 @@ object PlaidFixtures {
             merchantName = merchantName,
             checkNumber = checkNumber,
             personalFinanceCategory = personalFinanceCategory,
+        )
+    }
+
+    fun getTransferTestTransaction(
+        datetime: java.time.OffsetDateTime,
+        personalFinanceCategory: PersonalFinanceCategoryEnum,
+        amount: Double,
+        pendingTransactionId: String? = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        categoryId: String? = null,
+        category: List<String>? = null,
+        location: Location = Location(
+            address = null,
+            city = null,
+            region = null,
+            postalCode = null,
+            country = null,
+            lat = null,
+            lon = null,
+            storeNumber = null
+        ),
+        paymentMeta: PaymentMeta = PaymentMeta(
+            referenceNumber = null,
+            ppdId = null,
+            payee = null,
+            byOrderOf = null,
+            payer = null,
+            paymentMethod = null,
+            paymentProcessor = null,
+            reason = null
+        ),
+        accountOwner: String? = null,
+        name: String = "NAAAAAAME",
+        accountId: String = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+        isoCurrencyCode: String? = "USD",
+        unofficialCurrencyCode: String? = null,
+        date: LocalDate = LocalDate.of(2022, 9, 1),
+        pending: Boolean = false,
+        transactionId: String = Utilities.getRandomAlphabeticalString(30),
+        paymentChannel: PaymentChannel = PaymentChannel.other,
+        authorizedDate: LocalDate? = null,
+        authorizedDatetime: java.time.OffsetDateTime? = null,
+        transactionCode: TransactionCode? = null,
+        transactionType: Transaction.TransactionType? = null,
+        originalDescription: String? = null,
+        merchantName: String? = null,
+        checkNumber: String? = null,
+    ): Transaction {
+        return Transaction(
+            pendingTransactionId = pendingTransactionId,
+            categoryId = categoryId,
+            category = category,
+            location = location,
+            paymentMeta = paymentMeta,
+            accountOwner = accountOwner,
+            name = name,
+            accountId = accountId,
+            amount = amount,
+            isoCurrencyCode = isoCurrencyCode,
+            unofficialCurrencyCode = unofficialCurrencyCode,
+            date = date,
+            pending = pending,
+            transactionId = transactionId,
+            paymentChannel = paymentChannel,
+            authorizedDate = authorizedDate,
+            authorizedDatetime = authorizedDatetime,
+            datetime = datetime,
+            transactionCode = transactionCode,
+            transactionType = transactionType,
+            originalDescription = originalDescription,
+            merchantName = merchantName,
+            checkNumber = checkNumber,
+            personalFinanceCategory = PersonalFinanceCategory(personalFinanceCategory),
         )
     }
 
