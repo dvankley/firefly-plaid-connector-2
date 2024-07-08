7a11a9024ce3b6ec66ba661f9c737b839a782ae3
Dan van Kley <dvankley@gmail.com>
2024-07-07 21:14:35 -0400

    Revert kotlin Gradle plugin versions because they're causing build issues on my machine

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ val jacksonVersion: String by project
 plugins {
     id("org.springframework.boot") version "3.3.1"
     id("io.spring.dependency-management") version "1.1.5"
-    kotlin("jvm") version "2.0.0"
-    kotlin("plugin.spring") version "2.0.0"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
 }
 
 group = "net.djvk"
